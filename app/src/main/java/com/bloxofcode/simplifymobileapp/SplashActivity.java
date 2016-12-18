3a7368e7c88a104ae0fcbcc1b5cb8bc4c89f1073
bloxofcode <g.jayvee.apple@gmail.com>
2016-12-18 15:13:23 +0800

    Doing Login UI

--- app/src/main/java/com/bloxofcode/simplifymobileapp/SplashActivity.java
+++ app/src/main/java/com/bloxofcode/simplifymobileapp/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.bloxofcode.simplifymobileapp;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -18,5 +20,17 @@ public class SplashActivity extends AppCompatActivity {
         tv = (TextView) findViewById(R.id.textview);
         Typeface myCustomFont = Typeface.createFromAsset(getAssets(),"fonts/Roboto-Light.ttf");
         tv.setTypeface(myCustomFont);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 2000);
+
+//        Intent intent = new Intent(this, LoginActivity.class);
+//        startActivity(intent);
+//        finish();
     }
 }
